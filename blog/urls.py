acd62953fa9dd117fbe383a64a583817d1e7f3ee
tahany777 <tahanymohammed777@hotmail.com>
2023-01-21 22:55:33 +0300

    feat: add comments and read later

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # path('posts/<slug:slug>', views.post_detail, name="post-detail-page")#posts/my-first-app
     path('', views.StartingPageView.as_view(), name="starting-page"),
     path('posts', views.AllPostsView.as_view(), name="posts-page"),
-    path('posts/<slug:slug>', views.SinglePostView.as_view(), name="post-detail-page")#posts/my-first-app
+    path('posts/<slug:slug>', views.SinglePostView.as_view(), name="post-detail-page"),#posts/my-first-app
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
    
 ]
